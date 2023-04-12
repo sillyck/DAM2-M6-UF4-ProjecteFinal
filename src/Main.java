4524578014509b6d07e42237a9b0a1eaa7acfae5
Héctor Vallvé <hectorvallveg@gmail.com>
2023-04-12 17:58:00 +0200

    inici menu

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,9 @@
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		MenuInicial menuInicial = new MenuInicial();
+		
+		menuInicial.setVisible(true);
 
 	}
 
