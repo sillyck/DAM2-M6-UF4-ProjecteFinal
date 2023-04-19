12ad66bf86c1583b996cee1cd254e80271283be9
Héctor Vallvé <hectorvallveg@gmail.com>
2023-04-19 19:51:20 +0200

    coses

--- src/MenuInicial.java
+++ src/MenuInicial.java
@@ -1,3 +1,8 @@
+import java.awt.Container;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 
@@ -9,9 +14,46 @@ public class MenuInicial extends JFrame{
 		setVisible(true);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		JButton clients, llibres, ventes, consultar, sortir;
+		JButton clients = new JButton("Clients");
+		JButton llibres = new JButton("Llibres");
+		JButton ventes = new JButton("Ventes");
+		JButton consultar = new JButton("Consultar");
+		JButton sortir = new JButton("Sortir");
+		
+		Container cp = getContentPane();
+		cp.setLayout(new GridBagLayout());
+		
+		GridBagConstraints posicioBotoLlibres = new GridBagConstraints();
+		posicioBotoLlibres.gridx = 0;
+		posicioBotoLlibres.gridy = 0;
+		posicioBotoLlibres.insets = new Insets(0, 0, 100, 0);
+
+		GridBagConstraints posicioBotoClients = new GridBagConstraints();
+		posicioBotoClients.gridx = 0;
+		posicioBotoClients.gridy = 0;
+		posicioBotoClients.insets = new Insets(0, 0, 50, 0);
+
+		GridBagConstraints posicioBotoVentes = new GridBagConstraints();
+		posicioBotoVentes.gridx = 0;
+		posicioBotoVentes.gridy = 0;
+		posicioBotoVentes.insets = new Insets(50, 0, 0, 0);
+
+		GridBagConstraints posicioBotoConsulta = new GridBagConstraints();
+		posicioBotoConsulta.gridx = 0;
+		posicioBotoConsulta.gridy = 0;
+		posicioBotoConsulta.insets = new Insets(100, 0, 0, 0);
+
+		GridBagConstraints posicioBotoSortir = new GridBagConstraints();
+		posicioBotoSortir.gridx = 0;
+		posicioBotoSortir.gridy = 0;
+		posicioBotoSortir.insets = new Insets(100, 500, 0, 0);
 		
 		
+		cp.add(clients, posicioBotoClients);
+		cp.add(llibres, posicioBotoLlibres);
+		cp.add(ventes, posicioBotoVentes);
+		cp.add(consultar, posicioBotoConsulta);
+		cp.add(sortir, posicioBotoSortir);
 		
 		
 	}
